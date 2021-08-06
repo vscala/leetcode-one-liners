6d80e2522c0a347cc25b9bfd7801a3a284b23af4
Vincent Scala <vscala@uncc.edu>
2021-08-06 04:52:12 -0400

    One line solutions started
    largestIsland - scaffolding
    twoSum - complete

---
+++ Solution.py
@@ -0,0 +1,17 @@
+'''
+  August 2021 LeetCode Daily Challenges: https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/
+'''
+class Solution:
+    '''
+      Problem: https://leetcode.com/problems/making-a-large-island/
+      Solution description: ???
+      Complexity: ???
+    '''
+    #largestIsland = lambda s, g, i=0: max('''cur loc, next loc''') if i < len(grid)*len(grid[0]) else 0
+    
+    '''
+      Problem: https://leetcode.com/problems/two-sum/
+      Solution description: A recusive O(n^2) approach that iterates though each pair of elements in the array until a solution is found
+      Complexity: Time O(n^2)
+    '''
+    twoSum = lambda s,n,t,i=0: [i+1+n[i+1:].index(t-n[i]), i] if t-n[i] in n[i+1:] else s.twoSum(n,t,i+1)
